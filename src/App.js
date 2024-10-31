20e1f6b255e6e7e7a7e57911aa70b83723c74c59
eunbum <bum17822@naver.com>
2024-10-31 19:54:09 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '선문대 최고 아웃풋']); //[a,b]
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집';
+  
+  // function 제목바꾸기(){
+  //   var newArray = [...글제목];
+  //   newArray[0] = '여자 코트 추천';
+  //   글제목변경(newArray); 
+  // }
+
+  // function 순서바꾸기(){
+  //   var newArray = [...글제목];
+  //   var saveArray = newArray[0];
+  //   newArray[0] = newArray[2];
+  //   newArray[2] = saveArray;
+  //   글제목변경(newArray);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      {/* <button onClick={ 제목바꾸기 }>리스트 바꿔!</button>
+      <button onClick={ 순서바꾸기 }>순서 바꿔!</button> */}
+      <div className="list">
+        <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
+        <p>10월 31일 발행(이라네요)</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>상품이 아직 준비가 안되있어요</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>누굴까요???정말 궁금하네요</p>
+        <hr/>
+      </div>
+    
+      <Modal />
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
